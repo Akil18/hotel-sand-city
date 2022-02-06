bdd075be8221ffd7f9e64704f1604722b72da528
Akil18 <akiluddinchy@gmail.com>
2022-02-06 17:53:16 +0600

    added slider for rooms-lg-view and map

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  loop: true,
-  
-  effect: "cube",
-  grabCursor: true,
-  cubeEffect: {
-    shadow: false,
-    slideShadows: true,
-    shadowOffset: 2,
-    shadowScale: 0.8,
-  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "2",
+  spaceBetween: 30,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
